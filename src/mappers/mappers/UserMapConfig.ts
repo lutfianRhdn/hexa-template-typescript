73c5bc70a67b4156157df3b2c21dd6225485bf9c
Lutfian Rahdiansyah <lutfian.rhdn@gmail.com>
2026-03-25 02:35:49 +0700

    fix: correct import path in UserMapConfig.ts

--- src/mappers/mappers/UserMapConfig.ts
+++ src/mappers/mappers/UserMapConfig.ts
@@ -1,16 +1,15 @@
-import { EntityMapConfig } from '../adapters/postgres/repositories/Repository';
+import { EntityMapConfig } from '../../adapters/postgres/repositories/Repository';
 
-export const mapConfigs: Record<string, EntityMapConfig> = {
-	user: {
-		fields: [
-			{ dbField: 'id', entityField: 'id' },
-			{ dbField: 'name', entityField: 'name' },
-			{ dbField: 'username', entityField: 'username' },
-			{ dbField: 'password', entityField: 'password' },
-			{ dbField: 'role', entityField: 'role' },
-			{ dbField: 'isActive', entityField: 'isActive' },
-			{ dbField: 'createdAt', entityField: 'createdAt' },
-			{ dbField: 'updatedAt', entityField: 'updatedAt' },
-		],
-	},
+export const UserMapConfig: EntityMapConfig = {
+  fields: [
+    { dbField: 'id', entityField: 'id' },
+    { dbField: 'name', entityField: 'name' },
+    { dbField: 'username', entityField: 'username' },
+    { dbField: 'password', entityField: 'password' },
+    { dbField: 'role', entityField: 'role' },
+    { dbField: 'isActive', entityField: 'isActive' },
+    { dbField: 'createdAt', entityField: 'createdAt' },
+    { dbField: 'updatedAt', entityField: 'updatedAt' },
+  ],
+  relations: [],
 };
